4fbe7804d0383575eabcd3f89eb2ca9795959604
NilamKolekar-26 <nilam.kolekar@excellminds.com>
2025-10-31 11:26:49 +0530

    All splash screen logic should be handled within the Splash Controller so that the application opens directly to the dashboard if the user is already logged in, without requiring them to log in again.

--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -1,32 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:microfinance/routes/routes_string.dart';
+import 'package:microfinance/logic/controller/splashController.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-
-  @override
-  void initState() {
-    super.initState();
-
-    Future.delayed(const Duration(seconds: 2), () {
-      Get.toNamed(Routes.loginScreen);
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    Get.put(SplashController());
+
     return const Scaffold(
       backgroundColor: Colors.white,
       body: Center(
         child: Text(
-          "WelCome Micro Finance",
+          "Welcome Micro Finance",
           style: TextStyle(
             fontSize: 25,
             color: Colors.black,
