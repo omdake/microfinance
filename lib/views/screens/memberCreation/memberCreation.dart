995e8cffb0ca3d1dda36a352fa33948ebde0a1a2
NilamKolekar-26 <nilam.kolekar@excellminds.com>
2025-11-05 16:11:05 +0530

    membercreation bottomnavbar add and uploaddocumentsuccessscreen chnage

--- lib/views/screens/memberCreation/memberCreation.dart
+++ lib/views/screens/memberCreation/memberCreation.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:microfinance/common_widgets/custom_app_bar.dart';
+import 'package:microfinance/common_widgets/nav_bar.dart';
 import 'package:microfinance/logic/controller/memberCreation/memberCreationController.dart';
 import 'package:microfinance/views/screens/memberCreation/cibilDetailsScreen.dart';
 import 'package:microfinance/views/screens/memberCreation/personalDetails.dart';
@@ -28,6 +29,7 @@ class MemberCreationScreen extends StatelessWidget {
       appBar: appBarWithTitle1(
         title: "Create Member",
       ),
+      bottomNavigationBar: CustomBottomNavBar(),
       body: SafeArea(
         child: Column(
           children: [
