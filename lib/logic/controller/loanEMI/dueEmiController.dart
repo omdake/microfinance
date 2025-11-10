a551291598aa48d025d5bdfe733b0ec29ad68705
Nilam Kolekar <nilam.kolekar@excellminds.com>
2025-11-10 12:49:49 +0530

    Update dueEmiController.dart

--- lib/logic/controller/loanEMI/dueEmiController.dart
+++ lib/logic/controller/loanEMI/dueEmiController.dart
@@ -85,7 +85,7 @@ class DueEMIController extends GetxController {
         isIssue: true,
         message: "Internal Server Error. Please try again later.",
       );
-      else {
+      }else {
         final Map<String, dynamic> errormsg = jsonDecode(response.body);
         String msg = errormsg['message']['msg'];
         CustomSnackBar.show(isIssue: true, message: msg);
