de36cba53f5356ed87a3767e9a5d3a51ae1f494d
Nilam Kolekar <nilam.kolekar@excellminds.com>
2025-11-10 12:40:27 +0530

    Update dueEmiController.dart

--- lib/logic/controller/loanEMI/dueEmiController.dart
+++ lib/logic/controller/loanEMI/dueEmiController.dart
@@ -80,7 +80,12 @@ class DueEMIController extends GetxController {
       } else if (response.statusCode == 401) {
         await oauthService.handleExceptionLogout('AuthenticationError');
         CustomSnackBar.show(isIssue: true, message: "Authentication Error");
-      } else {
+      }else if (response.statusCode == 500) {
+      CustomSnackBar.show(
+        isIssue: true,
+        message: "Internal Server Error. Please try again later.",
+      );
+      else {
         final Map<String, dynamic> errormsg = jsonDecode(response.body);
         String msg = errormsg['message']['msg'];
         CustomSnackBar.show(isIssue: true, message: msg);
