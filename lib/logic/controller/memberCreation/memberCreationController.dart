77512b323cccc20c6b18c7c96c88dd8da345e984
Nilam Kolekar <nilam.kolekar@excellminds.com>
2025-11-18 15:36:43 +0530

    Update memberCreationController.dart

--- lib/logic/controller/memberCreation/memberCreationController.dart
+++ lib/logic/controller/memberCreation/memberCreationController.dart
@@ -150,44 +150,6 @@ class MemberCreationController extends GetxController {
       if (keyContext != null) {}
     }
   }
-
-  // void updateAgesFromDOB(String dobText) {
-  //   if (dobText.isEmpty) {
-  //     isDobSelected.value = false;
-  //     entryAge.value.text = '';
-  //     completedAge.value.text = '';
-  //     return;
-  //   }
-  //   if (!dobText.contains('-') || dobText.split('-').length != 3) {
-  //     entryAge.value.text = '';
-  //     completedAge.value.text = '';
-  //     isDobSelected.value = false;
-  //     return;
-  //   }
-  //   if (dobText.length == 10) {
-  //     try {
-  //       DateTime dob = DateFormat('dd-MM-yyyy').parseStrict(dobText);
-  //       DateTime today = DateTime.now();
-  //       int completed = today.year - dob.year;
-  //       if (today.month < dob.month ||
-  //           (today.month == dob.month && today.day < dob.day)) {
-  //         completed--;
-  //       }
-  //       int entry = completed + 1;
-  //       entryAge.value.text = entry.toString();
-  //       completedAge.value.text = completed.toString();
-  //       isDobSelected.value = true;
-  //     } catch (e) {
-  //       entryAge.value.text = '';
-  //       completedAge.value.text = '';
-  //       isDobSelected.value = false;
-  //     }
-  //   } else {
-  //     entryAge.value.text = '';
-  //     completedAge.value.text = '';
-  //     isDobSelected.value = false;
-  //   }
-  // }
   void updateAgesFromDOB(String dobText) {
     if (dobText.isEmpty) {
       isDobSelected.value = false;
@@ -806,10 +768,10 @@ class MemberCreationController extends GetxController {
         address.value.text = memberData.address ?? '';
         selectedAddressDocType.value = memberData.addressDocType ?? '';
         city.value.text = memberData.city ?? '';
-        consumerNumberController.value.text =
-            (memberData.consumerNo == 0 || memberData.pincode == null)
-                ? ''
-                : memberData.pincode.toString();
+        controller.showConsumerNumber.value = memberData.addressDocType == "ELECTRICITY BILL";
+        controller.consumerNumberController.value.text = controller.showConsumerNumber.value
+            ? (memberData.consumerNo?.toString() ?? '')
+            : '';
         pincode.value.text =
             (memberData.pincode == 0 || memberData.pincode == null)
                 ? ''
